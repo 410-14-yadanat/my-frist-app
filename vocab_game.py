616881c7e0d7b1a62d2b53d1956d903c5cc7d901
410-14-yadanat <54281@yupparaj.ac.th>
2026-08-25 13:19:05 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -101,4 +101,4 @@ if st.session_state.get("is_ended", False):
     show_result_dialog(ans1, ans2)
 
 st.divider()
-st.write("นางสาวดีใจ ยิ้มแย้ม เลขที่ 5 ม.4/5")
+st.write("นางสาวญาดาณัฐ พะริกะ เลขที่ 14 ม.4/10")
